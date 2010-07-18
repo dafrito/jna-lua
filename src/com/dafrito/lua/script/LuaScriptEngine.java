d5b50d35205fd3df2bbcd2f1dc40ff884153ef63
Aaron Faanes <dafrito@gmail.com>
2010-07-18 17:04:14 -0500

    LuaScriptEngine learned how to use lua_state, albeit dumbly

--- src/com/dafrito/lua/script/LuaScriptEngine.java
+++ src/com/dafrito/lua/script/LuaScriptEngine.java
@@ -10,12 +10,19 @@ import javax.script.ScriptEngineFactory;
 import javax.script.ScriptException;
 import javax.script.SimpleBindings;
 
+import lua.LuaLibrary;
+import lua.LuaLibrary.lua_State;
+
 public class LuaScriptEngine extends AbstractScriptEngine implements ScriptEngine {
 
+	private static final LuaLibrary lua = LuaLibrary.INSTANCE;
+	
 	private final LuaScriptEngineFactory factory;
+	private final lua_State state;
 
 	public LuaScriptEngine(LuaScriptEngineFactory luaScriptEngineFactory) {
 		this.factory = luaScriptEngineFactory;
+		this.state = LuaLibrary.INSTANCE.luaL_newstate();
 	}
 
 	public LuaScriptEngine() {
@@ -30,7 +37,11 @@ public class LuaScriptEngine extends AbstractScriptEngine implements ScriptEngin
 	@Override
 	public Object eval(String script, ScriptContext context)
 			throws ScriptException {
-		return 2;
+		lua.luaL_loadstring(this.state, script);
+		lua.lua_call(this.state, 0, 1);
+		double rv=lua.lua_tonumber(this.state, 1);
+		lua.lua_remove(this.state, 1);
+		return rv;
 	}
 
 	@Override
