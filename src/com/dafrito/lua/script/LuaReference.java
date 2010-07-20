f6a2fa6e2c0c30afbe638487af97b06d3b198940
Aaron Faanes <dafrito@gmail.com>
2010-07-20 16:18:47 -0500

    Simplified LuaReference. It no longer provides an explicit close() method

--- src/com/dafrito/lua/script/LuaReference.java
+++ src/com/dafrito/lua/script/LuaReference.java
@@ -1,72 +1,48 @@
 package com.dafrito.lua.script;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import com.google.common.base.FinalizablePhantomReference;
-import com.google.common.base.FinalizableReferenceQueue;
+import java.lang.ref.WeakReference;
 
 import lua.LuaLibrary;
 import lua.LuaLibrary.lua_State;
+
+import com.google.common.base.FinalizablePhantomReference;
+import com.google.common.base.FinalizableReferenceQueue;
 
 public class LuaReference {
 	private static final FinalizableReferenceQueue queue = new FinalizableReferenceQueue();
 	private static final LuaLibrary lua = LuaLibrary.INSTANCE;
 	
 	private final LuaBindings b;
-	private AtomicInteger ref;
-	private final LuaPhantomReference finalizer;
+	private final int ref;
 
 	public LuaReference(LuaBindings b) {
 		this.b = b;
-		this.ref = new AtomicInteger(lua.luaL_ref(b.getState(), LuaLibrary.LUA_REGISTRYINDEX));
-		this.finalizer = new LuaPhantomReference(this, queue);
-		check();
-	}
-
-	public LuaBindings getBindings() {
-		return this.b;
+		this.ref = lua.luaL_ref(b.getState(), LuaLibrary.LUA_REGISTRYINDEX);
+		new LuaPhantomReference(this, queue);
 	}
 
 	public void get() {
-		check();
-		lua.lua_rawgeti(b.getState(), LuaLibrary.LUA_REGISTRYINDEX, this.ref.get());
-	}
-
-	private void check() {
-		if (this.isClosed()) {
-			throw new RuntimeException();
-		}
-	}
-
-	public boolean isClosed() {
-		return this.ref.get() == LuaLibrary.LUA_REFNIL;
-	}
-	
-	public void close() {
-		this.finalizer.close();
+		lua.lua_rawgeti(b.getState(), LuaLibrary.LUA_REGISTRYINDEX, ref);
 	}
 
 	private static class LuaPhantomReference extends FinalizablePhantomReference<LuaReference> {
 
-		private AtomicInteger ref;
-		private lua_State state;
+		private final int ref;
+		private final WeakReference<lua_State> state;
 		
 		protected LuaPhantomReference(LuaReference referent, FinalizableReferenceQueue queue) {
 			super(referent, queue);
-			this.state = referent.getBindings().getState();
+			this.state = new WeakReference<lua_State>(referent.b.getState());
 			this.ref = referent.ref;
 		}
 		
-		public void close() {
-			int v = this.ref.getAndSet(LuaLibrary.LUA_REFNIL);
-			if(v != LuaLibrary.LUA_REFNIL) {
-				lua.luaL_unref(state, LuaLibrary.LUA_REGISTRYINDEX, v);	
-			}
-		}
-
 		@Override
 		public void finalizeReferent() {
-			this.close();
+			lua_State state = this.state.get();
+			if(state != null) {
+				this.state.clear();
+				lua.luaL_unref(state, LuaLibrary.LUA_REGISTRYINDEX, ref);	
+			}
 		}
 	}
 
